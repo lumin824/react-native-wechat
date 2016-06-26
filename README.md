ff6fa409b21bd01c3dcea22ec6ab7b88d36a7ff8
lumin <lumin824@163.com>
2016-06-27 00:05:40 +0800

    ios

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # react-native-wechat
+
+
+android
+
+AndroidManifest.xml->
+<activity android:name=".wxapi.WXEntryActivity" android:label="@string/app_name" android:exported="true" />
+
+public class WXEntryActivity extends WXSampleEntryActivity {
+}
+
+gradle.properties
+WECHAT_APP_ID=wx111111111
+
+
+ios
+
+ Info->URL Types 添加 Identifier -> weixin URL Schemes -> wx111111111
--- ios/WechatModule.m
+++ ios/WechatModule.m
@@ -1,11 +1,79 @@
 
 #import "RCTBridgeModule.h"
+#import <UIKit/UIKit.h>
+#import "WXApi.h"
 
-@interface WechatModule : NSObject<RCTBridgeModule>
+@interface WechatModule : NSObject<RCTBridgeModule,WXApiDelegate>
 
+@property(nonatomic, copy) RCTPromiseResolveBlock authReqResolve;
+@property(nonatomic, copy) RCTPromiseRejectBlock authReqReject;
 @end
 
 @implementation WechatModule
+
+-(instancetype) init
+{
+    self = [super init];
+    if(self){
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleOpenURL:) name:@"RCTOpenURLNotification" object:nil];
+        
+        NSString* appId = nil;
+        NSArray* list = [[[NSBundle mainBundle] infoDictionary] valueForKey:@"CFBundleURLTypes"];
+        for(NSDictionary* item in list){
+            NSString* name = item[@"CFBundleURLName"];
+            if([name isEqualToString:@"weixin"])
+            {
+                NSArray* schemes = item[@"CFBundleURLSchemes"];
+                if(schemes.count > 0){
+                    appId = schemes[0];
+                    break;
+                }
+            }
+        }
+        
+        [WXApi registerApp:appId];
+    }
+    return self;
+}
+
+- (void)handleOpenURL:(NSNotification *)note
+{
+    NSDictionary *userInfo = note.userInfo;
+    NSURL *url = [NSURL URLWithString:userInfo[@"url"]];
+    [WXApi handleOpenURL:url delegate:self];
+}
+
+-(dispatch_queue_t) methodQueue
+{
+    return dispatch_get_main_queue();
+}
+
+-(void) onReq:(BaseReq *)req
+{
+}
+
+-(void) onResp:(BaseResp *)resp
+{
+    if([resp isKindOfClass:[SendAuthResp class]]){
+        SendAuthResp * authResp = (SendAuthResp *) resp;
+        
+        if(authResp.code){
+            if(self.authReqResolve){
+                self.authReqResolve(authResp.code);
+                self.authReqResolve = nil;
+                self.authReqReject = nil;
+            }
+        }else{
+            if(self.authReqReject){
+                self.authReqReject(@"cancel", @"cancel", nil);
+                self.authReqResolve = nil;
+                self.authReqReject = nil;
+            }
+        }
+        
+    }
+}
+
 
 RCT_EXPORT_MODULE()
 
@@ -13,7 +81,12 @@ RCT_EXPORT_METHOD(sendAuthReq:(NSString*)scope state:(NSString*)state
                   resolve:(RCTPromiseResolveBlock) resolve
                   reject:(RCTPromiseRejectBlock) reject)
 {
-    resolve(@"ok");
+    SendAuthReq* req = [[SendAuthReq alloc]init];
+    req.scope = scope;
+    req.state = state;
+    self.authReqResolve = resolve;
+    self.authReqReject = reject;
+    [WXApi sendReq:req];
 }
 
 @end
